4f4692d019b1320c413abcfeb82d912511adf5ab
jsemric <jakubsemric@gmail.com>
2017-11-15 15:18:47 +0100

    adding people

--- src/evac_ca.cpp
+++ src/evac_ca.cpp
@@ -143,6 +143,10 @@ void EvacCA::add_people(int people_count) {
         people.push_back(empty_cells.back());
         empty_cells.pop_back();
     }
+
+    for (auto i : people) {
+        get_cell(i).cell_type = Person;
+    }
 }
 
 EvacCA EvacCA::load_from_pixmap(const std::string &filename) {
