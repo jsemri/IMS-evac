e75f1d7a87ba5afc28a8cfa2c90cf1a19bef8e1e
gargantophob <gargantophob@gmail.com>
2017-11-15 16:27:28 +0100

    test2

--- src/bitmap.d
+++
@@ -1,2 +0,0 @@
-src/bitmap.o: src/bitmap.cpp src/bitmap.h src/evac_ca.h \
- 3rdparty/bitmap_image.hpp
--- src/evac_ca.d
+++
@@ -1,2 +0,0 @@
-src/evac_ca.o: src/evac_ca.cpp src/evac_ca.h src/bitmap.h \
- 3rdparty/bitmap_image.hpp
