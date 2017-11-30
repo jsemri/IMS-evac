0eecda4cec80cf94dbfbddfb0a667fc472c779e7
gargantophob <gargantophob@gmail.com>
2017-11-30 19:45:17 +0100

    Experimenting, p.2

--- script.sh
+++ script.sh
@@ -13,11 +13,13 @@ cases=(
     #'D-3.bmp'
     #'D-4.bmp'
     #'D-5.bmp'
-	#'E-1.bmp'
+    #'D-5b.bmp'
+    #'D-5c.bmp'
+    #'E-1.bmp'
     #'E-2.bmp'
-    'E-3.bmp'
-    'E-4.bmp'
-    'E-5.bmp'
+    #'E-3.bmp'
+    #'E-4.bmp'
+    #'E-5.bmp'
 );
 
 for (( i=0; i<${#cases[@]}; i++ ));
