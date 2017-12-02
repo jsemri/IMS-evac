63aacead879a543214dfe13d7eae5d1a09967084
gargantophob <gargantophob@gmail.com>
2017-12-02 17:49:22 +0100

    Experiments, p.7

--- script.sh
+++ script.sh
@@ -2,25 +2,25 @@
 exe='./evac';
 dir='./experiments';
 
-pedestrians=350;
-smoke=1;
-simulations=2000;
+pedestrians=200;
+smoke=3;
+simulations=1000;
 delay=0;
 opt="-t $delay -p $pedestrians -s $smoke -r $simulations";
 
 cases=(
     #'D1.bmp'
     #'D2.bmp'
-    'D3.bmp'
+    #'D3.bmp'
     #'D4.bmp'
-    #'D5.bmp'
+    'D5.bmp'
     #'D5-3.bmp'
     #'D5-4.bmp'
     #'E1.bmp'
     #'E2.bmp'
     #'E3.bmp'
     #'E4.bmp'
-    #'E5.bmp'
+    'E5.bmp'
 );
 
 for (( i=0; i<${#cases[@]}; i++ ));
